7c453457a28a82c0334dff4e7ebe47d1a254d8b8
hansani k.j.m. <it21222672@my.sliit.lk>
2023-12-04 10:12:57 +0530

    the commit

---
+++ tea_mngmnt/src/main/java/com/teamngmnt/teamngmntDButil.java
@@ -0,0 +1,15 @@
+package com.teamngmnt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class teamngmntDButil {
+	public static List<teamngmnt> validate(String userName , String password){
+		
+		
+		ArrayList<teamngmnt> tea = new ArrayList<>();
+		return tea;
+	}
+	
+
+}
